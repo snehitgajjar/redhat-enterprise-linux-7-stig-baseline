145365bb4a99ed02e281ccba24380bbea63d966d
Aaron Lippold <lippold@gmail.com>
2020-04-07 17:28:14 -0400

    Update V-71861.rb

--- controls/V-71861.rb
+++ controls/V-71861.rb
@@ -181,6 +181,7 @@ GUI.
     end
   else
     describe "The system does not have GNOME installed" do
+      impact 0.0
       skip "The system does not have GNOME installed, this requirement is Not
         Applicable."
     end
