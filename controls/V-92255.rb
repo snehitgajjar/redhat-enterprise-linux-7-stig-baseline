32302b963b593138dfe5f91e8a49fdc8c191ffca
Lesley Kimmel <lesley.j.kimmel@users.noreply.github.com>
2020-04-10 19:21:00 +0000

    Added new control for V-92255.
    - Resolves #9
    
    Signed-off-by: Lesley Kimmel <lesley.j.kimmel@users.noreply.github.com>

--- controls/V-92255.rb
+++ controls/V-92255.rb
@@ -62,5 +62,32 @@ with the Authorizing Official.
   tag fix_id: "F-98477r1_fix"
   tag cci: ["CCI-001263"]
   tag nist: ["SI-4 (5)", "Rev_4"]
+
+  custom_hips = input('custom_hips')
+
+  if ! custom_hips
+    describe package('MFEhiplsm') do
+      it { should be_installed }
+    end
+    describe processes(/hipclient/) do
+      it { should exist }
+    end
+  else
+    # Special case for SELinux
+    sel_mode = command('getenforce').stdout.strip
+    custom_hips_daemon = input('custom_hips_daemon')
+    max_daemon_processes = input('max_daemon_processes')
+
+    describe.one do
+      describe "SELinux mode" do
+        subject { sel_mode }
+        it { should cmp 'Enforcing' }
+      end
+      describe processes(/#{custom_hips_daemon}/) do
+        it { should exist }
+        its('count') { should be < max_daemon_processes }
+      end
+    end
+  end
 end
 
